5136ca75ab16ed698a32cb4d8f00df0210923b3f
azree <azreenasir99@gmail.com>
2024-11-13 00:46:37 +0800

    create Context Api

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -4,8 +4,12 @@ import Message from './Message';
 import styles from './CityList.module.css';
 import CityItem from './CityItem';
 
+import { useCities } from '../contexts/CitiesContext';
 
-function CityList({cities, isLoading}) {
+
+function CityList() {
+    const {cities, isLoading} = useCities()
+
     if(isLoading) return <Spinner/>
 
     if(!cities.length) return <Message message='Add your first city by clicking on a city on the Map'/>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import estlint from "vite-plugin-eslint";
+// import estlint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), estlint()],
+  plugins: [react()],
+  // plugins: [react(), estlint()],
 });
